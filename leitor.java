5cc9792f4de1d01df480356fecc4561a94732fd9
segalv <anaclarasvp@usp.br>
2024-10-30 23:48:45 -0300

    Criação leitor de arquivo

---
+++ leitor.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class leitor {
+    public String[] listaPalavras;
+    public leitor(String path){
+        List <String> linhas = new ArrayList<>(); //criando o String[] de forma genérica para usar as funcionalidades de util.List
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) { //readline "quebra" ao encontrar um \n. quando o arquivo chegar ao fim, será lido 'null'
+                linhas.add(linha); // Adiciona cada linha lida com sucesso ao arranjo
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        // Convertendo o ArrayList (de tipo genérico) para um array de Strings efetivamente para auxiliar em manipulações futuras
+        this.listaPalavras = linhas.toArray(new String[0]);
+    }
+}
